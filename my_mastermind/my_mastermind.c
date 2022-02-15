64a68b1186ee5bd20b2412865e15f0e568042173
noelcserepy <noelcserepy@gmail.com>
2022-02-15 13:10:50 +0100

    done mastermind

---
+++ my_mastermind/my_mastermind.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct GameState
+{
+    char code[10];
+    int code_len;
+    int attempts;
+    char guess[10];
+    int well_placed;
+    int misplaced;
+} GameState;
+
+void compare_codes(GameState *state)
+{
+    int well = 0;
+    int mis = 0;
+
+    for (int i = 0; i < state->code_len; i++)
+    {
+        for (int j = 0; j < state->code_len; j++)
+        {
+            if (state->guess[j] == state->code[i])
+                mis++;
+        }
+        if (state->guess[i] == state->code[i])
+        {
+            well++;
+            mis--;
+        }
+    }
+
+    state->well_placed = well;
+    state->misplaced = mis;
+}
+
+int mastermind(GameState *state)
+{
+
+    printf("Will you find the secret code?\n---\n");
+
+    for (int i = 0; i < state->attempts; i++)
+    {
+        printf("Round %d\n", i);
+        scanf("%s", state->guess);
+
+        compare_codes(state);
+
+        if (state->well_placed == state->code_len)
+        {
+            printf("Congratz, you did it!");
+            return 0;
+        }
+
+        printf("Well placed pieces: %d\nMisplaced pieces: %d\n---\n", state->well_placed, state->misplaced);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    GameState state;
+    state.attempts = 10;
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-t") == 0)
+        {
+            state.attempts = atoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            strcpy(state.code, argv[i + 1]);
+            state.code_len = strlen(argv[i + 1]);
+        }
+    }
+
+    mastermind(&state);
+
+    return 0;
+}
---
+++ my_mastermind/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+ind_well_placed(int *well_placed, char *code, char *guess)
+// {
+// }
+
+// void mastermind(char *code, int code_len, int attempts)
+// {
+
+//     printf("Will you find the secret code?\n---\n");
+
+//     for (int i = 0; i < attempts; i++)
+//     {
+//         printf("Round %d\n", i);
+//         scanf("%s", &guess);
+
+//         find_well_placed();
+//         find_misplaced();
+//     }
+// }
