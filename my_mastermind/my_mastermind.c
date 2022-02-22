bf4423069b08b4303727675930b3264792a0a422
Noël Cserépy <noelcserepy@gmail.com>
2022-02-22 12:29:12 +0100

    fixed input bugs, added messages

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 typedef struct GameState
 {
@@ -11,6 +12,7 @@ typedef struct GameState
     char guess[10];
     int well_placed;
     int misplaced;
+    int retry;
 } GameState;
 
 void compare_codes(GameState *state)
@@ -36,6 +38,29 @@ void compare_codes(GameState *state)
     state->misplaced = mis;
 }
 
+int check_code(GameState *state)
+{
+    if (strlen(state->guess) != 4)
+    {
+        printf("\nThe code must be EXACTLY 4 characters long.");
+        state->retry = 1;
+    }
+    int has_alpha = 0;
+    for (int j = 0; j < strlen(state->guess); j++)
+    {
+        if (isdigit(state->guess[j]) == 0)
+        {
+            has_alpha = 1;
+        }
+    }
+    if (has_alpha == 1)
+    {
+        printf("\nThe code must be numerical.");
+        state->retry = 1;
+    }
+    printf("Try again.\n---\n");
+}
+
 int mastermind(GameState *state)
 {
 
@@ -45,12 +70,19 @@ int mastermind(GameState *state)
     {
         printf("Round %d\n", i);
         scanf("%s", state->guess);
+        check_code(state);
+        if (state->retry == 1)
+        {
+            state->retry = 0;
+            i--;
+            continue;
+        }
 
         compare_codes(state);
 
         if (state->well_placed == state->code_len)
         {
-            printf("Congratz, you did it!");
+            printf("Congratz, you did it!\n");
             return 0;
         }
 
@@ -64,6 +96,7 @@ int main(int argc, char *argv[])
     GameState state;
     state.attempts = 10;
     state.code_len = 4;
+    state.retry = 0;
 
     // Generate a random secret code
     srand(time(0));
@@ -78,10 +111,20 @@ int main(int argc, char *argv[])
         if (strcmp(argv[i], "-t") == 0)
         {
             state.attempts = atoi(argv[i + 1]);
+            if (state.attempts < 1)
+            {
+                printf("The number of attempts must be a positive integer.\n");
+                return 0;
+            }
         }
         if (strcmp(argv[i], "-c") == 0)
         {
             strcpy(state.code, argv[i + 1]);
+            check_code(&state);
+            if (state.retry == 1)
+            {
+                return 0;
+            }
         }
     }
 
