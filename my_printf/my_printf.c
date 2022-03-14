5f80226cda2f9bf59b9ea0d3d44f98fb017c9d95
Noël Cserépy <noelcserepy@gmail.com>
2022-03-14 17:05:19 +0100

    Added some test cases and count

--- my_printf/my_printf.c
+++ my_printf/my_printf.c
@@ -4,23 +4,33 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int my_putchar(char c)
+int my_putchar(char c, int *count)
 {
     return write(1, &c, 1);
+    *count++;
 }
 
 // write some code for \ escapes
 // return number of chars printed
 
-void print_arg(char *type_char, va_list va_list)
+void print_string(char *s, int *count)
 {
+    int j = 0;
+    while (s[j])
+    {
+        my_putchar(s[j], count);
+        j++;
+    }
 }
 
 char my_printf(char *format, ...)
 {
+    int *count = malloc(sizeof(int));
     char *current;
-    unsigned int i;
+    int i;
+    unsigned int u;
     char *s;
+    char buffer[50];
     va_list valist;
     va_start(valist, format);
 
@@ -28,7 +38,7 @@ char my_printf(char *format, ...)
     {
         while (*current != '%')
         {
-            my_putchar(*current);
+            my_putchar(*current, count);
             current++;
         }
         current++;
@@ -37,35 +47,49 @@ char my_printf(char *format, ...)
         {
         case 'd':
             i = va_arg(valist, int);
-            my_putchar(i);
+            itoa(i, buffer, 10);
+            print_string(buffer, count);
             break;
         case 'o':
+            u = va_arg(valist, int);
+            itoa(u, buffer, 8);
+            print_string(buffer, count);
+            break;
+        case 'u':
+            u = va_arg(valist, unsigned int);
+            itoa(u, buffer, 10);
+            print_string(buffer, count);
+            break;
+        case 'x':
+            u = va_arg(valist, unsigned int);
+            itoa(u, buffer, 16);
+            print_string(buffer, count);
+            break;
+        case 'p':
             i = va_arg(valist, int);
-            my_putchar(i);
+            itoa(i, buffer, 16);
+            print_string(buffer, count);
             break;
         case 's':
             s = va_arg(valist, char *);
-            int j = 0;
-            while (j < strlen(s))
-            {
-                my_putchar(s[j]);
-                j++;
-            }
-            break;
-        case 'x':
-            i = va_arg(valist, int);
-            my_putchar(i);
+            print_string(s, count);
             break;
         case 'c':
             i = va_arg(valist, int);
-            my_putchar(i);
+            my_putchar(i, count);
             break;
         }
     }
     va_end(valist);
+    return *count;
 }
 
 int main(int ac, char **av)
 {
-    my_printf("hoi %s %p %d", "gaggi", "1234123", "3");
+    void *a;
+    int count;
+    count = printf("OG: %d %o %u %x %p %s %c\n", -1234123, 12, -13, 16, a, "saliduli", 'k');
+    printf("%d\n", count);
+    count = my_printf("Mine: %d %o %u %x %p %s %c\n", -1234123, 12, -13, 16, a, "saliduli", 'k');
+    printf("%d\n", count);
 }
